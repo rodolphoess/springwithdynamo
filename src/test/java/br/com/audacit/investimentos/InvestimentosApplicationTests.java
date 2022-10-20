44889568c6adea89c9f463db34593d76c74712ff
Rodolpho Sousa <rodolphoerick90@gmail.com>
2022-10-19 21:34:45 -0300

    Adicionando projeto no git

---
+++ src/test/java/br/com/audacit/investimentos/InvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.audacit.investimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
