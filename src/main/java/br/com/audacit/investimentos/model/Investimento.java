ec7bf8753c43a129a7ff8f20ad65115a5a128e4b
Rodolpho Sousa <rodolphoerick90@gmail.com>
2022-11-09 21:50:28 -0300

    feat: removendo import inutilizado

--- src/main/java/br/com/audacit/investimentos/model/Investimento.java
+++ src/main/java/br/com/audacit/investimentos/model/Investimento.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
