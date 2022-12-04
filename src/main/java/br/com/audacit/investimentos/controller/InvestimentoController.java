7c68367d33c309d61e9f463eb8cd0dc469c7308d
Rodolpho Sousa <rodolphoerick90@gmail.com>
2022-12-04 16:17:19 -0300

    feat: adicionando anotação no controller e criação do repository

--- src/main/java/br/com/audacit/investimentos/controller/InvestimentoController.java
+++ src/main/java/br/com/audacit/investimentos/controller/InvestimentoController.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
+@RestController
 @RequestMapping("/investimento")
 public class InvestimentoController {
 
---
+++ src/main/java/br/com/audacit/investimentos/database/InvestimentoRepository.java
@@ -0,0 +1,11 @@
+package br.com.audacit.investimentos.database;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class InvestimentoRepository {
+}
