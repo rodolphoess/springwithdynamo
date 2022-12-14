67289d2c9c40848217bdaf2f0315c8a60572a8cd
Rodolpho Sousa <rodolphoerick90@gmail.com>
2022-12-14 15:34:41 -0300

    feat: adição de chamadas ao service no controller e modificações de assinatura no service

--- src/main/java/br/com/audacit/investimentos/controller/InvestimentoController.java
+++ src/main/java/br/com/audacit/investimentos/controller/InvestimentoController.java
@@ -18,25 +18,37 @@ import java.util.UUID;
 @RequestMapping("/investimento")
 public class InvestimentoController {
 
-    private InvestimentoService investimentoService;
+    private final InvestimentoService investimentoService;
 
     @PostMapping("/investir")
     public ResponseEntity<Object> investir(@Valid @RequestBody InvestimentoRequest investimento) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(Object.class);
+        log.info("solicitacao_investimento_recebida={}", investimento.toString());
+        var identificacaoInvestimento = investimentoService.injetarDinheiro(investimento);
+        log.info("solicitacao_investimento_concluida_com_sucesso={}", identificacaoInvestimento);
+        return ResponseEntity.status(HttpStatus.CREATED).body(identificacaoInvestimento);
     }
 
     @PostMapping("/retirar")
     public ResponseEntity<Object> retirar(@Valid @RequestBody RetiradaRequest retirada) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(Object.class);
+        log.info("solicitacao_retirada_recebida={}", retirada.toString());
+        var identificacaoRetirada = investimentoService.retirarDinheiro(retirada);
+        log.info("solicitacao_retirada_concluida_com_sucesso={}", identificacaoRetirada);
+        return ResponseEntity.status(HttpStatus.CREATED).body(identificacaoRetirada);
     }
 
     @GetMapping("/saldo/mensal")
     public ResponseEntity<Object> saldoMensal(@RequestParam("mes") String mes) {
-        return ResponseEntity.status(HttpStatus.OK).body(Object.class);
+        log.info("solicitacao_recuperacao_saldo_mensal={}", mes);
+        var saldoMes = investimentoService.saldoMensal(mes);
+        log.info("saldo_mensal_recuperado_com_sucesso=R$ {}", saldoMes);
+        return ResponseEntity.status(HttpStatus.OK).body(saldoMes);
     }
 
     @DeleteMapping("/cancelar/movimentacao")
     public ResponseEntity<Object> cancelarMovimentacao(@RequestParam("movimentacao") UUID identificacaoMovimentacao) {
+        log.info("solicitacao_cancelamento_movimentacao_recebida={}", identificacaoMovimentacao);
+        investimentoService.cancelarInvestimento(identificacaoMovimentacao);
+        log.info("cancelamento_movimentacao_realizado_com_sucesso");
         return ResponseEntity.status(HttpStatus.OK).body("Movimentação cancelada com sucesso!");
     }
 
--- src/main/java/br/com/audacit/investimentos/service/InvestimentoService.java
+++ src/main/java/br/com/audacit/investimentos/service/InvestimentoService.java
@@ -1,5 +1,7 @@
 package br.com.audacit.investimentos.service;
 
+import br.com.audacit.investimentos.dto.in.InvestimentoRequest;
+import br.com.audacit.investimentos.dto.in.RetiradaRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -10,24 +12,23 @@ import java.util.UUID;
 @Service
 public class InvestimentoService {
 
-    public UUID injetarDinheiro() {
+    public UUID injetarDinheiro(InvestimentoRequest investimento) {
         log.info("injetando_dinheiro");
         return UUID.randomUUID();
     }
 
-    public UUID retirarDinheiro() {
+    public UUID retirarDinheiro(RetiradaRequest retirada) {
         log.info("retirando_dinheiro");
         return UUID.randomUUID();
     }
 
-    public BigDecimal saldoMensal() {
+    public BigDecimal saldoMensal(String mesSaldo) {
         log.info("saldo_mensal");
         return BigDecimal.ONE;
     }
 
-    public UUID cancelarInvestimento() {
+    public void cancelarInvestimento(UUID identificacaoMovimentacao) {
         log.info("cancelando_investimento");
-        return UUID.randomUUID();
     }
 
 }
